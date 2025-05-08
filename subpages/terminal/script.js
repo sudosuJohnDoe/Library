f90eae5ace8410a2eefa22fc1c3c23d2e7d7dfab
sudosuJohnDoe <tsavel2011@gmail.com>
2025-05-08 12:10:17 +0500

    Add files via upload

---
+++ subpages/terminal/script.js
@@ -0,0 +1,25 @@
+const inputField = document.getElementById('userInput');
+inputField.focus();
+inputField.addEventListener('blur', function() {
+    setTimeout(() => {
+        inputField.focus();
+    }, 0);
+});
+document.getElementById('userInput').addEventListener('keypress', function(event) {
+	if (event.key === 'Enter') {
+		checkInput();
+	}
+});
+function checkInput() {
+	const userInput = document.getElementById('userInput').value;
+	const output = document.getElementById('outputText');
+	if (userInput === 'help') {
+		output.textContent = 'open - open file';
+	}
+	else if (userInput === 'open man.png') {
+		window.open('media/img/man.png');
+	}
+	else {
+		output.textContent = 'bash: command not found';
+	}	
+}
